305aa11210ab4fbf4bd5aefa54a32a727b795ae6
huschpuscheli <bogner.f@googlemail.com>
2022-07-29 21:28:01 +0200

    edit output text

--- Come_With_Me_If_You_Want_To_Live.py
+++ Come_With_Me_If_You_Want_To_Live.py
@@ -155,7 +155,7 @@ if input_date < pd.Timestamp("2021-12-31 23:59:59"):
 elif input_date > pd.Timestamp("2021-12-31 23:59:59"):
     input_date = pd.to_datetime(input_date)
     f_crime_count = round(forecast.loc[forecast['ds'] == input_date]['yhat'].values[0], 2)
-    st.markdown("## There are {} crimes forecast on that day against individuals with the selected demographics".format(f_crime_count))
+    st.markdown("## There are {} crimes forecast on that day against individuals with the selected demographics.".format(f_crime_count))
     
     
     

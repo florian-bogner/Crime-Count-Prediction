8a2651d14866d939caefd6c2af46f8d75d87b417
huschpuscheli <bogner.f@googlemail.com>
2022-07-29 15:39:06 +0200

    edit data input

--- Come_With_Me_If_You_Want_To_Live.py
+++ Come_With_Me_If_You_Want_To_Live.py
@@ -76,7 +76,7 @@ with st.sidebar:
 ##########################################################################################
 
 url='https://drive.google.com/file/d/16zQFS6M0aooYCMjzq0qfqIipTd8KLbO_/view?usp=sharing'
-url='https://drive.google.com/uc?id=' + url.split('/')[-2]
+path = 'https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
 df = pd.read_csv(url)
 st.dataframe(df.head(1))
 

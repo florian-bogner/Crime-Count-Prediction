4790eeaeb06fa275c40ffbed96f09bb3aa38bd7a
huschpuscheli <bogner.f@googlemail.com>
2022-07-29 20:08:33 +0200

    edit output text

--- Come_With_Me_If_You_Want_To_Live.py
+++ Come_With_Me_If_You_Want_To_Live.py
@@ -10,7 +10,13 @@ from prophet import Prophet
 ##########################################################################################
 
 with st.sidebar:
-    input_boro = st.selectbox('Select NYC borough:', ('Manhattan', 'Bronx', 'Queens', 'Brooklyn', 'Staten Island'))
+    input_boro = st.selectbox(
+         'Select NYC borough:',
+         ('Manhattan',
+          'Bronx',
+          'Queens',
+          'Brooklyn',
+          'Staten Island'))
     
     if input_boro == 'Manhattan':
         boro = 'MANHATTAN'
@@ -23,9 +29,18 @@ with st.sidebar:
     else:
         boro = 'STATEN ISLAND'
     
-    age = st.selectbox('Select age group:',('<18', '18-24', '25-44', '45-64', '65+'))
+    age = st.selectbox(
+         'Select age group:',
+         ('<18',
+          '18-24',
+          '25-44',
+          '45-64',
+          '65+'))
     
-    input_gender = st.selectbox('Select sex:', ('Female', 'Male'))
+    input_gender = st.selectbox(
+         'Select sex:',
+         ('Female',
+          'Male'))
     
     if input_gender == 'Female':
         gender = 'F'
@@ -33,7 +48,13 @@ with st.sidebar:
         gender = 'M'
 
     input_ethn = st.selectbox(
-         'Select ethnicity:', ('Black', 'White', 'White Hispanic', 'Asian/Pacific Islander', 'Black Hispanic', 'American Indian/Alaskan Native'))
+         'Select ethnicity:',
+         ('Black',
+          'White',
+          'White Hispanic',
+          'Asian/Pacific Islander',
+          'Black Hispanic',
+          'American Indian/Alaskan Native'))
 
     if input_ethn == 'Black':
         ethn = 'BLACK'
@@ -46,7 +67,10 @@ with st.sidebar:
     else:
         ethn = 'AMERICAN INDIAN/ALASKAN NATIVE'
     
-    input_date = st.date_input("Select date:", min_value = datetime.date(2006, 1, 1), max_value = datetime.date(2023, 12, 31))
+    input_date = st.date_input(
+        "Select date:",
+        min_value = datetime.date(2006, 1, 1),
+        max_value = datetime.date(2023, 12, 31))
 
 ##########################################################################################
 
@@ -54,6 +78,9 @@ with st.sidebar:
 def load_data():
     df = pd.read_csv('top17crimes_9col.csv')
     df['start_datetime_of_event'] = pd.to_datetime(df['start_datetime_of_event'])
+    #df_fel_misd = df.loc[(df['offense_level'] == 'FELONY') | (df['offense_level'] == 'MISDEMEANOR')].copy()
+    #twenty_crimes = df_fel_misd['description'].value_counts().head(17).index.to_list()
+    top_twenty = df #df_fel_misd.loc[df['description'].isin(twenty_crimes)].copy()
     return top_twenty
 
 top_twenty = load_data()
@@ -70,17 +97,37 @@ def df_slicer(boro,
               gender,
               ethn
              ):
-    slice_ = top_twenty.loc[(top_twenty['boro'] == boro) & (top_twenty['victim_age'] == age) & (top_twenty['victim_sex'] == gender) & (top_twenty['victim_race'] == ethn)]
+    slice_ = top_twenty.loc[
+        (top_twenty['boro'] == boro) &
+        (top_twenty['victim_age'] == age) &
+        (top_twenty['victim_sex'] == gender) &
+        (top_twenty['victim_race'] == ethn)
+    ]
     slice_ = slice_.drop(columns = ['description', 'boro', 'lat', 'lon', 'victim_age', 'victim_race', 'victim_sex'])
     
-    slice_['ymd'] = (slice_['start_datetime_of_event'].dt.strftime('%Y') + "-" + slice_['start_datetime_of_event'].dt.strftime('%m') + "-" + slice_['start_datetime_of_event'].dt.strftime('%d') )
+    slice_['ymd'] = (
+    slice_['start_datetime_of_event'].dt.strftime('%Y') + "-" +
+    slice_['start_datetime_of_event'].dt.strftime('%m') + "-" +
+    slice_['start_datetime_of_event'].dt.strftime('%d')
+    )
     
-    slice_ymd = (slice_[['ymd', 'id']].groupby(slice_['ymd']).agg({'id': 'count'}).reset_index().copy())
+    slice_ymd = (
+        slice_[['ymd', 'id']]
+        .groupby(slice_['ymd'])
+        .agg({'id': 'count'})
+        .reset_index()
+        .copy()
+    )
     slice_ymd = slice_ymd.reindex(columns={'id', 'ymd'})
     slice_ymd = slice_ymd.rename(columns={'id' :'y', 'ymd': 'ds'})
     return slice_ymd
 
 slice_ymd = df_slicer(boro, age, gender, ethn)
+
+#buffer = io.StringIO()
+#slice_ymd.info(buf=buffer)
+#s = buffer.getvalue()
+#st.text(s)
 
 ##########################################################################################
 
@@ -109,3 +156,8 @@ elif input_date > pd.Timestamp("2021-12-31 23:59:59"):
     input_date = pd.to_datetime(input_date)
     f_crime_count = round(forecast.loc[forecast['ds'] == input_date]['yhat'].values[0], 2)
     st.markdown("## There are {} crimes forecast on that day against individuals with the selected demographics".format(f_crime_count))
+    
+    
+    
+    
+    

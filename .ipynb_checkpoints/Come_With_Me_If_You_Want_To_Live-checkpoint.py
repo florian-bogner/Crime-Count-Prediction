00341aa1621937d6ab5265d1a2b26148140beeeb
huschpuscheli <bogner.f@googlemail.com>
2022-07-29 15:35:20 +0200

    edit data input

--- .ipynb_checkpoints/Come_With_Me_If_You_Want_To_Live-checkpoint.py
+++ .ipynb_checkpoints/Come_With_Me_If_You_Want_To_Live-checkpoint.py
@@ -107,19 +107,7 @@ def df_slicer(boro,
         (top_twenty['victim_sex'] == gender) &
         (top_twenty['victim_race'] == ethn)
     ]
-    slice_ = slice_.drop(columns = [
-        'description',
-        'boro',
-        'lat',
-        'lon',
-        'victim_age',
-        'victim_race',
-        'victim_sex'
-                                   ]
-                        )
-    
-    'id', 'start_datetime_of_event', 'description', 'boro', 'lat', 'lon',
-       'victim_age', 'victim_race', 'victim_sex'
+    slice_ = slice_.drop(columns = ['description', 'boro', 'lat', 'lon', 'victim_age', 'victim_race', 'victim_sex'])
     
     slice_['ymd'] = (
     slice_['start_datetime_of_event'].dt.strftime('%Y') + "-" +
